c73bd5e4a07800146969d70621e267a6de500cb2
chiaoymir <yingchiao.chen@gmail.com>
2022-07-02 14:24:37 +0800

    complete week5 exercise

--- week5/farm/src/main.rs
+++ week5/farm/src/main.rs
@@ -66,17 +66,37 @@ fn get_input_numbers() -> VecDeque<u32> {
     numbers
 }
 
+fn get_number(queue_ref: Arc<Mutex<VecDeque<u32>>>) -> Option<u32> {
+    let mut queue = queue_ref.lock().unwrap();
+    queue.pop_front()
+}
+
 fn main() {
     let num_threads = num_cpus::get();
     println!("Farm starting on {} CPUs", num_threads);
     let start = Instant::now();
 
     // TODO: call get_input_numbers() and store a queue of numbers to factor
+    let input_numbers = get_input_numbers();
+    let num_len = input_numbers.len();
+    let num_queue: Arc<Mutex<VecDeque<u32>>> = Arc::new(Mutex::new(input_numbers));
 
     // TODO: spawn `num_threads` threads, each of which pops numbers off the queue and calls
     // factor_number() until the queue is empty
+    let mut threads = Vec::new();
+    for _ in 0..num_len {
+        let num_queue_ref = num_queue.clone();
+        threads.push(thread::spawn(move || {
+            if let Some(num) = get_number(num_queue_ref) {
+                factor_number(num);
+            }
+        }));
+    }
 
     // TODO: join all the threads you created
+    for handle in threads {
+        handle.join().expect("Panic occurred in thread!");
+    }
 
     println!("Total execution time: {:?}", start.elapsed());
 }
